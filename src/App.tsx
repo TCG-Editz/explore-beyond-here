2ac5f33931842a653686a5bad17ee1bd119d07cb
TCG-Editz <prakratcs@gmail.com>
2025-09-13 16:42:20 +0530

    linked p1

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import TropicalParadise from "./pages/TropicalParadise";
+import TropicalParadise from "@/pages/TropicalParadise";
+
 
 const queryClient = new QueryClient();
 
