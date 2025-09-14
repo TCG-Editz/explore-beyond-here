d5d2f6fb19d18da4fb7593638a652057785504c8
TCG-Editz <prakratcs@gmail.com>
2025-09-14 22:41:55 +0530

    navigation fixed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation = () => {
   }, []);
 
   const navItems = [
-    { label: "Monastries", href: "/tropical-paradise" },
+    { label: "Monastries", href: "tropical-paradise" },
     { label: "Services", href: "#services" },
     { label: "About", href: "#about" },
     { label: "Contact", href: "#contact" }
